b66c6e6f19fc154e4caa593abe7c542db3f3f23d
Dan Mandel <dan19926@gmail.com>
2020-01-06 20:41:39 -0500

    Improve stack implementation

--- src/datastructures/stack/index.ts
+++ src/datastructures/stack/index.ts
@@ -1,18 +1,21 @@
+/* A stack is a data structure where insertion and deletion of items
+ * takes place at one end in LIFO order.
+ */
 export class Stack<T> {
 	private data: T[] = [];
 
 	/* Add a node in O(1) time. */
-	push(node: T): void {
+	public push(node: T): void {
 		this.data.push(node);
 	}
 
 	/* Remove the last inserted node in O(1) time and return it, or undefined if there are no nodes remaining. */
-	pop(): T | undefined {
+	public pop(): T | undefined {
 		return this.data.pop();
 	}
 
 	/* Return the number of elements in the stack in O(1) time. */
-	size(): number {
+	public size(): number {
 		return this.data.length;
 	}
 }
